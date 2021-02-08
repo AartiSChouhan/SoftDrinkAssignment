a5a756228ca7a60ba52f4626b1c5a4e391eb624b
AartiSChouhan <78337711+AartiSChouhan@users.noreply.github.com>
2021-02-08 11:16:50 +0530

    SoftDrink(6th Jan Assignment)

---
+++ SoftDrinkDemo.java
@@ -0,0 +1,60 @@
+package day9;
+ interface SoftDrink{
+	String taste(String taste);
+	String colour(String colr);
+ }
+	
+	class Maaza implements SoftDrink{
+
+		@Override
+		public String taste(String tst) {
+			// TODO Auto-generated method stub
+			System.out.println("Taste of the Maaza is : "+tst);
+			return tst;
+		}
+
+		@Override
+		public String colour(String clr) {
+			// TODO Auto-generated method stub
+			System.out.println("Colr of the Maaza is : "+clr);
+			
+			return  clr;
+		}
+		
+	}
+	class Fizz implements SoftDrink{
+
+		@Override
+		public String taste(String tst) {
+			// TODO Auto-generated method stub
+			System.out.println("Taste of the Fizz is : "+tst);
+			return tst;
+		}
+
+		@Override
+		public String colour(String clr) {
+			// TODO Auto-generated method stub
+			System.out.println("Colr of the Fizz is : "+clr);
+			
+			return  clr;
+		}
+		
+	}
+
+	
+
+public class SoftDrinkDemo {
+	public static void main(String[] args) {
+		SoftDrink d=new Maaza();
+		SoftDrink d1=new Fizz();
+
+		d.colour("Yellow");
+		d.taste("Mango");
+		
+		d1.colour("Brown");
+		d1.taste("Apple");
+		
+		
+	}
+
+}
